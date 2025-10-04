4c4e8c2fbbff14872cdbd8068849f71d184eff39
Rokas <rokas.krisciunas@mif.stud.vu.lt>
2025-10-04 13:17:28 +0300

    Fix Supabase environment variables

--- netlify/functions/supabaseConfig.js
+++ netlify/functions/supabaseConfig.js
@@ -3,12 +3,8 @@
 // In deletePotato.js
 
 const SUPABASE_URL = process.env['https://bjjddiqplwyjwiyjadbk.supabase.co'];
-const SUPABASE_SERVICE_ROLE_KEY = process.env['sb_secret_y2TpY7iAa8mCkyku-1ILEg_H-eT5rcb'];
+const SUPABASE_SERVICE_ROLE_KEY = process.env['sb_secret_y2TpY7iAa8mCkyku-1ILEg_H-eT5rcb;
 
-
-
-
-// These two lines are different:
 const AUTH_HEADERS = {
     // 1. Supabase's Custom API Key Header: Just the key itself
     'apikey': SUPABASE_SERVICE_ROLE_KEY,
